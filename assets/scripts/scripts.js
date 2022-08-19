dec6bbb80fe770f99c31c6b0d4da8552237880c7
Lucas Garcia Buixens <Lucasgb@hotmail.com>
2022-08-19 15:27:27 -0300

    Proyecto terminado v1.0

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,12 @@
+
+document.querySelector('.year-copy').innerText = new Date().getFullYear();
+
+
+
+    // let vinculo = document.getElementsByClassName('link-interno');
+    // let winSize = window.innerWidth;
+    // if (winSize < 992) {
+    //     vinculo.href = "#consulta-bajo-991px";
+    //     }
+    
+    
